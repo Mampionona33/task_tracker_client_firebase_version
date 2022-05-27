47fb35e6acb46b8084468ada32608b61b7459621
Mampionona33 <ramamps33@gmail.com>
2022-05-27 08:59:14 +0300

    rename app.jsx to App.js

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function App(params) {
+  return <h1>Place holder for App</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
