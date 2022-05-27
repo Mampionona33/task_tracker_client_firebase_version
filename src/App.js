df9a2467558e9f00ec59ce84b1b811103a430957
Mampionona33 <ramamps33@gmail.com>
2022-05-27 09:00:35 +0300

    add comment in line 4 at App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// first entry point of the application
+
 export default function App(params) {
   return <h1>Place holder for App</h1>;
 }
