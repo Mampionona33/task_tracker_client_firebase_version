10661b72961ce62d6333ef00c3dd298fa0b11333
Mampionona33 <ramamps33@gmail.com>
2022-05-30 13:41:04 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import Dashboard from './Page/Dashboard';
+import History from './Page/History';
 
 export default function App() {
-  return <div>s</div>;
+  return (
+    <div>
+      <Dashboard />
+      <History />
+    </div>
+  );
 }
