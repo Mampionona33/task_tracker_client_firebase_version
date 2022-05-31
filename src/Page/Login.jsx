a0588a554b9d6145c9fe442d7402542b4037d180
Mampionona33 <ramamps33@gmail.com>
2022-05-31 15:43:08 +0300

    remove p in login.jsx

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 export default function Login() {
+  const handleSingIng = () => {
+    console.log('login');
+  };
   return (
     <React.Fragment>
       <button className='button' onClick={handleSingIng}>
         Signe In With Google
       </button>
-      <p>{userName}</p>
     </React.Fragment>
   );
 }
