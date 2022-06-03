084304675d529814567f4f24db2c62ff687dcdff
Mampionona33 <ramamps33@gmail.com>
2022-06-03 11:14:47 +0300

    finish to create route in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './Page/Dashboard';
+import Login from './Page/Login';
+import History from './Page/History';
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='login' element={<Login />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='history' element={<History />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 export default function Login() {
   return (
     <>
-      <button>Sing In</button>
+      <button>SingIn with Google</button>
     </>
   );
 }
