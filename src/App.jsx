e9f26d948ae2e3ea42140ead05c9a28b2bfe2940
Mampionona33 <ramamps33@gmail.com>
2022-06-03 11:06:59 +0300

    cleanup project

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import Dashboard from './Page/Dashboard';
-import Login from './Page/Login';
-import History from './Page/History';
-import { PrivateRoute } from './Page/PrivateRoute';
-import { useContext } from 'react';
-import { AuthContext } from './Firebase/context';
 
 export default function App() {
-  const { user } = useContext(AuthContext);
-  console.log(user);
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          index
-          path='/'
-          element={!user ? <Login /> : <Navigate to={'/dashboard'} replace />}
-        />
-        <Route
-          path='login'
-          element={!user ? <Login /> : <Navigate to={'/dashboard'} replace />}
-        />
-        <Route path='*' element={'404 Not found'} />
-        <Route element={<PrivateRoute user={user} />}>
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/history' element={<History />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <></>;
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 import App from './App';
-import { AuthProvider } from './Firebase/context';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
-root.render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
-);
+root.render(<App />);
 // used to make component hot reload
 if (module.hot) {
   module.hot.accept();
