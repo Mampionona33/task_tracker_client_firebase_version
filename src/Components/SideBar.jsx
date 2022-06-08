58c678d59e522b02c2fcbbec8a59a335c1bff266
Mampionona33 <ramamps33@gmail.com>
2022-06-08 16:31:44 +0300

    presque

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/SideBar.scss';
 
-export default function SideBar() {
+export default function SideBar({ isOpen }) {
+  const sidebarClasseName = isOpen ? ' sidebar--open' : ' sidebar--close';
   return (
-    <div className='sidebar'>
+    <div className={'sidebar' + sidebarClasseName}>
       <Link to={'/history'}>history</Link>
       <Link to={'/dashboard'}>dashbord</Link>
     </div>
