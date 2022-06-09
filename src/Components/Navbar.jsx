f5828b66e52496efbf2b78a3fb2c73d79514be90
Mampionona33 <ramamps33@gmail.com>
2022-06-09 16:28:28 +0300

    finish to setup navbar menu click

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Navbar.scss';
 
 export default function Navbar() {
+  const [toggleSideBar, setToggleSideBar] = useState(false);
+
   return (
     <div className='navbar'>
-      <button className='navbar__menu__button'>
-        <span className='material-icons-round'>menu</span>
+      <button
+        className='navbar__menu__button'
+        onClick={() => setToggleSideBar(!toggleSideBar)}
+      >
+        {!toggleSideBar ? (
+          <span className='material-icons-round'>menu</span>
+        ) : (
+          <span class='material-icons-round'>close</span>
+        )}
       </button>
     </div>
   );
