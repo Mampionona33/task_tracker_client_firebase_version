04fb9d11c0d4200661f4b2f75854fd0bbf7446e8
Mampionona33 <ramamps33@gmail.com>
2022-06-13 12:08:20 +0300

    Add AnimatePresence in sidebar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -28,61 +28,61 @@ export default function Navbar() {
   }, [user]);
 
   return (
-    <AnimatePresence exitBeforeEnter>
-      <div className='bars'>
-        <div className='navbar'>
-          <button
-            type='button'
-            className='navbar__menu__button'
-            onClick={() => setToggleSideBar(!toggleSideBar)}
-          >
-            {!toggleSideBar ? (
-              <motion.span
-                key={'iconMenu'}
-                variants={buttonMenuVariants}
-                initial='hidden'
-                animate='animate'
-                className='material-icons-round'
-              >
-                menu
-              </motion.span>
-            ) : (
-              <motion.span
-                key={'iconClose'}
-                variants={buttonCloseVariants}
-                initial='hidden'
-                animate='animate'
-                exit='exit'
-                className='material-icons-round'
-              >
-                close
-              </motion.span>
-            )}
-          </button>
-          <motion.h1
-            key={'navbarTitle'}
-            variants={navbarTitleVariants}
-            initial='hidden'
-            animate='visible'
-            exit='exit'
-            className='navbar__title'
-          >
-            {location.pathname.slice(1).charAt(0).toUpperCase() +
-              location.pathname.slice(2)}
-          </motion.h1>
-          <button
-            type='button'
-            onClick={() => setSignOutBtnOpen(!signOutBtnOpen)}
-          >
-            {user && (
-              <img
-                src={userPhotoUrl}
-                alt={user.displayName.slice(0, user.displayName.indexOf(' '))}
-              />
-            )}
-          </button>
-        </div>
-        <div className='navbar__container'>
+    <div className='bars'>
+      <div className='navbar'>
+        <button
+          type='button'
+          className='navbar__menu__button'
+          onClick={() => setToggleSideBar(!toggleSideBar)}
+        >
+          {!toggleSideBar ? (
+            <motion.span
+              key={'iconMenu'}
+              variants={buttonMenuVariants}
+              initial='hidden'
+              animate='animate'
+              className='material-icons-round'
+            >
+              menu
+            </motion.span>
+          ) : (
+            <motion.span
+              key={'iconClose'}
+              variants={buttonCloseVariants}
+              initial='hidden'
+              animate='animate'
+              exit='exit'
+              className='material-icons-round'
+            >
+              close
+            </motion.span>
+          )}
+        </button>
+        <motion.h1
+          key={'navbarTitle'}
+          variants={navbarTitleVariants}
+          initial='hidden'
+          animate='visible'
+          exit='exit'
+          className='navbar__title'
+        >
+          {location.pathname.slice(1).charAt(0).toUpperCase() +
+            location.pathname.slice(2)}
+        </motion.h1>
+        <button
+          type='button'
+          onClick={() => setSignOutBtnOpen(!signOutBtnOpen)}
+        >
+          {user && (
+            <img
+              src={userPhotoUrl}
+              alt={user.displayName.slice(0, user.displayName.indexOf(' '))}
+            />
+          )}
+        </button>
+      </div>
+      <div className='navbar__container'>
+        <AnimatePresence exitBeforeEnter>
           {toggleSideBar && (
             <motion.div
               key={'sidebar'}
@@ -94,9 +94,9 @@ export default function Navbar() {
               <SideBar />
             </motion.div>
           )}
-          {signOutBtnOpen && <SingOutButton />}
-        </div>
+        </AnimatePresence>
+        {signOutBtnOpen && <SingOutButton />}
       </div>
-    </AnimatePresence>
+    </div>
   );
 }
