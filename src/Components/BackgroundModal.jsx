86fc057aaecc84f5fbad0b36f66b8162ddc87748
Mampionona33 <ramamps33@gmail.com>
2022-06-13 13:35:57 +0300

    remove import in BackgroundModal.jsx

--- src/Components/BackgroundModal.jsx
+++ src/Components/BackgroundModal.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/BackgroundModal.scss';
-import Sliding from './Sliding';
 
 export default function BackgroundModal({ children }) {
   return <div className='modal'>{children}</div>;
