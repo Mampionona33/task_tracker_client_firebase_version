55389a62a1a26bb987533d6996c63695e698cb79
Mampionona33 <ramamps33@gmail.com>
2022-06-15 14:51:57 +0300

    Card.jsx : change div to before and after

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,11 +4,9 @@ import '../styles/Card.scss';
 export default function Card({ title, children }) {
   return (
     <div className='card'>
-      <div className='card__sup'></div>
       <h1 className='card__title'>{title}</h1>
       <hr />
       <div className='element'>{children}</div>
-      <div className='card__inf'></div>
     </div>
   );
 }
